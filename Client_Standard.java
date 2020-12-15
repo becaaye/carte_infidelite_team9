220eb78fa4a251d8db29ce2fe4b56ec6bc0078e3
Bécaye Badiane <becaye00@gmail.com>
2020-12-14 22:13:25 -0500

    Delete Client_Standard.java

--- Client_Standard.java
+++
@@ -1,8 +0,0 @@
-public class Client_Standard extends Client {
-
-	public void demanderAvantageStandard() {
-		// TODO - implement Client_Standard.demanderAvantageStandard
-		throw new UnsupportedOperationException();
-	}
-
-}
